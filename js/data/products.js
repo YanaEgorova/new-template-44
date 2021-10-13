496ec65e79b043d398db8cf3207507e716924758
Yana <yanaegorova69.3@gmail.com>
2021-10-13 22:24:46 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,155 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Gaming Surface',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Seamless, Anti-fray stitched edges.',
+            },
+            {
+                text: 'Densely woven surface for accurate Optical tracking.',
+            },
+            {
+                text: 'Natural rubber textured underside stays stable.',
+            },
+            {
+                text: 'Four sizes suitable for most play spaces and styles.',
+            },
+            {
+                text: 'Portable and durable.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Pet Carrier',
+        price: 29.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'Perfect for traveling in style and comfort by plane or car, for an adventure or just a trip to the vet.',
+            },
+            {
+                text: 'Patented spring wire frame allows the rear end of the carrier to be pushed down several inches to conform to under-seat requirements.',
+            },
+            {
+                text: 'Locking zippers for safety and mesh windows for ventilation.',
+            },
+            {
+                text: 'Padded adjustable, no-slip carrying strap, seat belt/luggage strap, machine-washable faux lambskin liner, and rear pocket for convenient storage of treats, leash, or bags.',
+            },
+            {
+                text: 'Approved for use on most airlines. Size medium is for pets up to 16" L x 10" H and up to 16 lbs Medium carrier measures 17” L x 11” W x 10.5” H Do NOT select the carrier size based on weight only. Choose the carrier size based on your pet’s measurements first, then make sure not to exceed the maximum weight limit..Carrier is intended for safe and comfortable travel; the pet will not have excess room to move around.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Teeth Model',
+        price: 64.90,
+        image: './img/img3.png',
+        description: [
+            {
+                text: 'Standard Dental implant tooth model.',
+            },
+            {
+                text: '28 Removable Teeth.',
+            },
+            {
+                text: 'Translucent and soft rubber gingiva.',
+            },
+            {
+                text: 'Training and practicing.',
+            },
+            {
+                text: 'Used for Teaching and Doctor-patient Communication.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png', './img/img3-4.png', './img/img3-5.png', './img/img3-6.png', './img/img3-7.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Tap Timer',
+        price: 98.90,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'Provides highly efficient automatic watering for small areas from a hose tap.',
+            },
+            {
+                text: 'Program two zones from the same hose tap.',
+            },
+            {
+                text: 'Bluetooth enabled for total wireless smartphone control via app.',
+            },
+            {
+                text: 'Control an unlimited number of devices for ultimate flexibility.',
+            },
+            {
+                text: 'Manual push-button feature ensures easy operation without a smartphone.Alkaline batteries included.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Foldable Creeper',
+        price: 119.98,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'Transforms with no tools in seconds.',
+            },
+            {
+                text: '40" Steel frame construction, Seat Size: 14" x 12-1/4"',
+            },
+            {
+                text: 'Padded seat and headers.',
+            },
+            {
+                text: '450 pounds rated capacity.',
+            },
+            {
+                text: 'Six 3" polyurethane, oil resistant, full bearing casters for easy movement.',
+            },
+        ],
+        images: ['./img/img5-1.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Gaming Chair',
+        price: 148.00,
+        image: './img/img6.png',
+        description: [
+            {
+                title: 'Adjustable Backrest',
+                text: 'This stylish gaming chair is designed with a backrest that adjusts between 4 different positions, allowing you to relax upright or recline in comfort.',
+            },
+            {
+                title: 'Comfortable & Fun',
+                text: 'Whether for intense gaming or much needed relaxation, this chair puts you in a comfortable position that you can enjoy for hours!',
+            },
+            {
+                title: 'Versatile Use',
+                text: 'A lightweight design lets you take the chair from room to room, using it for activities such as reading, gaming, watching TV, and so much more!',
+            },
+            {
+                title: 'Durable Build',
+                text: 'Sturdy frame withstands up to 285 pounds, while linen fabric and thick, tufted cushioning help ensure lasting comfort. The machine-washable base cover also prevents scratches and scuffs to your floor.',
+            },
+            {
+                title: '360-Degree Swivel + Foldable Design',
+                text: 'Enjoy restrictionless mobility with a steel ball-bearing system that provides 360-degree functionality; plus, it folds down for easy storage after use.',
+            },
+            {
+                title: 'Overall Dimensions',
+                text: '25"(L) x 24"(W) x 34.75"(H); Weight Capacity: 285 lbs.',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-2.png','./img/img6-3.png','./img/img6-4.png'],
+    },
+]
